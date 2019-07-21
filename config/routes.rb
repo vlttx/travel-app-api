b145d23c28d88f9b92075fb001989b363252a293
VBTX <jv.svedaite@gmail.com>
2019-07-21 18:17:29 -0500

    edited the delete route mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 post "/api/v1/login", to: "api/v1/sessions#create"  
-delete "/api/v1/login", to: "api/v1/sessions#destroy" 
+delete "/api/v1/logout", to: "api/v1/sessions#destroy" 
 get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"  
 namespace :api do
  namespace :v1 do	
@@ -11,7 +11,7 @@ namespace :api do
   resources :comments
   resources :attractions
   resources :users do 
-  	resourses :trips, only: [:index]
+  	resources :trips, only: [:index]
   end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
