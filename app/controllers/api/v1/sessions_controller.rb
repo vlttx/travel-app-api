5d329cf754ed96ada1d5ece265bf70e9197d97bc
VBTX <jv.svedaite@gmail.com>
2019-07-21 18:18:55 -0500

    no major changes

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::SessionsController < ApplicationController
+
+
 	def create
 		@user = User.find_by(username: params[:session][:username])
 
 		if @user && @user.authenticate(params[:session][:password])
 			session[:user_id] = @user.id
 			# setting a user id for the session which is grabbed by app controller
-			render json:@user
+			# render json   :@user
+
+			render json: @user
+
+
 		else
 			render json: {
 				error: "Invalid Credentials"
