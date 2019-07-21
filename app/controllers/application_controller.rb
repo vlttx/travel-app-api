0cf5ac9f8a7ac33abea04661906d75a582018abe
VBTX <jv.svedaite@gmail.com>
2019-07-21 08:40:15 -0500

    add include ::ActionController::Cookies in order to save session cookies for keeping current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
-	
+	 include ::ActionController::Cookies
 		def current_user
-			User.find(session[:user_id])
+			User.find_by(id: session[:user_id])
+			# finding the user by using the user_id in the sessions controller create action
 		end
 
 		def logged_in?
