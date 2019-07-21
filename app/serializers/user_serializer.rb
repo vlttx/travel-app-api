1f219c4638f30e3687439857947bb37389eb521f
VBTX <jv.svedaite@gmail.com>
2019-07-21 13:48:48 -0500

    made trips available in user JSON

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :name, :hometown
-	# has_many :trips
+  has_many :trips
 	# has_many :visits, through: :trips
  #    has_many :attractions, through: :trips
  #    has_many :locations, through: :trips
