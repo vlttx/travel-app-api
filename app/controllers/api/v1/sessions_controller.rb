29acbb3061cb0b300fe597d32e8cbf00b95804e7
VBTX <jv.svedaite@gmail.com>
2019-07-20 19:49:10 -0500

    add get_current_user method that I use in routes

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,14 +3,23 @@ class Api::V1::SessionsController < ApplicationController
 		@user = User.find_by(username: params[:session][:username])
 
 		if @user && @user.authenticate(params[:session][:password])
+			session[:user_id] = @user.id
+			# setting a user id for the session which is grabbed by app controller
 			render json:@user
-
 		else
 			render json: {
 				error: "Invalid Credentials"
 			}
 	end
+end
 
-	def login
-	 end
+	def get_current_user
+		if logged_in?
+			render json: current_user
+		else
+			render json: {
+				error: "No one logged in"
+			}
+		end
+	end
 end
