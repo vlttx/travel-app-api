b45668e143c8fc91ec344abab8496b4293ee2d58
VBTX <jv.svedaite@gmail.com>
2019-07-21 13:48:31 -0500

    add trips under users

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ delete "/api/v1/login", to: "api/v1/sessions#destroy"
 get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"  
 namespace :api do
  namespace :v1 do	
-  resources :users
   resources :locations
   resources :visits
   resources :trips
   resources :comments
   resources :attractions
+  resources :users do 
+  	resourses :trips, only: [:index]
+  end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
