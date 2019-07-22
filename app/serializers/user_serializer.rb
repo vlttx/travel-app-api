509b14750ea46a03c1ea5505241d70e72c842dc9
VBTX <jv.svedaite@gmail.com>
2019-07-22 10:38:07 -0500

    comment out locations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :name
   belongs_to :hometown, serializer: HometownSerializer
-  # has_many :trips
-  
+  has_many :trips, serializer: TripSerializer
+  # has_many :locations, through: :trips
 	# has_many :visits, through: :trips
  #    has_many :attractions, through: :trips
- #    has_many :locations, through: :trips
+  
 	# has_many :comments
 end
