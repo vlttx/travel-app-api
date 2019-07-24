1dc868da5e11695967995e35fe502d1de61a6693
VBTX <jv.svedaite@gmail.com>
2019-07-24 10:10:49 -0500

    add trip id

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :name, :start_date, :end_date, :imageurl, :user_id
+  attributes :id, :name, :start_date, :end_date, :imageurl, :user_id
   belongs_to :user
   # belongs_to :location, serializer: LocationSerializer
 end
